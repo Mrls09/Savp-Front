41ab0cf77a2b8dcf7c64fc432b63a61f3f834311
Iván Samuel Mata <104937992+IvanMataHalcon@users.noreply.github.com>
2023-12-10 18:09:07 -0600

    Update

--- src/shared/components/UserNavbar.jsx
+++ src/shared/components/UserNavbar.jsx
@@ -2,8 +2,20 @@ import '../../utils/styles/UserNavbar.css'
 import { FaRegUserCircle, FaSearch } from 'react-icons/fa'
 import { IoIosSettings } from 'react-icons/io'
 import { BsStack } from 'react-icons/bs'
+import FeatherIcon from 'feather-icons-react/build/FeatherIcon'
+import { Button } from 'react-bootstrap'
+import { useContext } from 'react'
+import { AuthContext } from '../../modules/auth/authContext'
+import { useNavigate } from 'react-router-dom'
 
 const UserNavbar = () => {
+    const { dispatch } = useContext(AuthContext);
+    const navigation = useNavigate();
+    const handleLogout = () => {
+        dispatch({ type: "LOGOUT" });
+        navigation("/auth", { replace: true });
+        localStorage.removeItem("user");
+    };
 
     return (
         <div className='UserNav'>
@@ -19,6 +31,7 @@ const UserNavbar = () => {
                 }} style={{ height: 50, width: 32, marginBottom: 0 }} />
                 <FaRegUserCircle className='DataIcon' onClick={() => {
                 }} style={{ height: 28, width: 25, marginBottom: 0 }} />
+                <Button variant='none' onClick={handleLogout}><FeatherIcon icon={'log-out'} /></Button>
             </div>
         </div>
     );
